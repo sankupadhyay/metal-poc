853d407ba74c3755348937fd0e3191c1bd16e609
Rohit S Jadhav <rohitjadhav420@gmail.com>
2018-07-10 02:26:04 +0530

    [FIX] partial event handling for offices

--- src/Theme.js
+++ src/Theme.js
@@ -28,6 +28,7 @@ class Theme extends Component {
     attached() {
         console.log("    Network Calls Started   ");
 
+        this.makeWebServiceRequest('https://2f862b76-eb7c-4682-877a-ef7da823eb1d.mock.pstmn.io/offices','get','offices');
         this.makeWebServiceRequest('https://8d0ab2a9-2f00-4040-9721-345b7b2f17cf.mock.pstmn.io/lang','get','languages');
         this.makeWebServiceRequest('https://e159277c-1dce-473f-a147-d777c1308421.mock.pstmn.io/industries','get','industries');
         this.makeWebServiceRequest('https://e159277c-1dce-473f-a147-d777c1308421.mock.pstmn.io/testimonials','get','testimonials');
@@ -36,6 +37,7 @@ class Theme extends Component {
 
     willUpdate(changes, propsChanges) {
         console.log(this.languages);
+        console.log(this.offices);
     }
 
     willReceiveState(changes) {
--- src/components/menu-bar/MenuBar.js
+++ src/components/menu-bar/MenuBar.js
@@ -3,6 +3,7 @@
 import templates from './MenuBar.soy.js';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
+import Ajax from 'metal-ajax';
 
 import './menu-bar-drop-down/MenuBarDropDown';
 import './MenuBar.scss';
@@ -14,8 +15,23 @@ class MenuBar extends Component {
         });
     }
     handleOffice(event){
+        let countryCode = '';
         if (event.office) {
-            console.log(event.office);
+            if ( event.office == "Newyork Office") {
+                countryCode = 'us';
+            }
+            if ( event.office == "London Office") {
+                countryCode = 'uk';
+            }
+            Ajax.request(`https://2f862b76-eb7c-4682-877a-ef7da823eb1d.mock.pstmn.io/office/${countryCode}`, 'get')
+            .then(xhrResponse => {
+                var jsonData  = JSON.parse(xhrResponse.responseText);
+                this.officeData = jsonData;
+                console.log(this.officeData);
+            })
+            .catch(error => {
+                console.log(error);
+            });
         }
     }
 }
@@ -26,6 +42,9 @@ MenuBar.STATE = {
     },
     offices: {
         value:[]
+    },
+    officeData: {
+        
     }
 }
 
